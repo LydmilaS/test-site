607227647131528ac9b57058cbd2994204daef65
LydmilaS <lydmilazoloto@gmail.com>
2016-01-01 10:11:39 +0200

    change site(responsive)

---
+++ Ethanol/js/menu.js
@@ -0,0 +1,14 @@
+//mrnu
+	$(document).ready(function(){
+
+	    $("#button-toggle").click(function(){
+	        $("#button-toggle").hide();
+	        $("#close-menu").show();
+	    });
+	    $("#close-menu").click(function(){
+	        $("#close-menu").hide();
+	        $("#button-toggle").show();
+	    });
+
+	});
+		
